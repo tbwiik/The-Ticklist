58989ed860f6fd1ccf025ad4fcff6d4306222712
Torbjørn Wiik <torwii22@gmail.com>
2023-01-05 19:11:55 +0100

    Initial Commit

---
+++ The Ticklist/The_TicklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_TicklistApp.swift
+//  The Ticklist
+//
+//  Created by Torbjørn Wiik on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct The_TicklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ The TicklistUITests/The_TicklistUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  The_TicklistUITestsLaunchTests.swift
+//  The TicklistUITests
+//
+//  Created by Torbjørn Wiik on 05/01/2023.
+//
+
+import XCTest
+
+final class The_TicklistUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
