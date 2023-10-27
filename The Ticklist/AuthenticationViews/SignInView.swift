6a2f829723e1f5aed487bf94c4f859c24b09c8e6
Torbjørn Wiik <torwii22@gmail.com>
2023-10-27 20:50:12 +0200

    feat: add Sign-In page

---
+++ The Ticklist/AuthenticationViews/SignInView.swift
@@ -0,0 +1,50 @@
+//
+//  SignInView.swift
+//  The Ticklist
+//
+//  Created by Torbjørn Wiik on 27/10/2023.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    
+    @EnvironmentObject var authViewModel: AuthenticationViewModel
+    
+    // Get environment var that can dismiss views
+    @Environment(\.dismiss) private var dismiss
+    
+    private func signInEmailPasswd() {
+        Task {
+            if await authViewModel.signInEmailPasswd() == true {
+                dismiss()
+            }
+        }
+    }
+    
+    var body: some View {
+        
+        VStack {
+            TextField("Email", text: $authViewModel.email)
+            TextField("Password", text: $authViewModel.passwd)
+        }
+        Button (action: signInEmailPasswd){
+            
+            if !authViewModel.isAuthenticating(){
+                Text("Sign In")
+            } else {
+                Text("Authenticating...")
+            }
+            
+        }
+        .sheet(item: $authViewModel.errorWrapper, onDismiss: {
+            authViewModel.reset()
+        }) { wrapped in
+            ErrorView(errorWrapper: wrapped)
+        }
+    }
+}
+
+#Preview {
+    SignInView()
+}
